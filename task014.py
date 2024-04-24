d3fab5f949bbad23afce535dca5a469d9e2092da
Nailya Musaeva <musaevana@mail.ru>
2024-04-24 13:32:25 +0300

    замена номера авто

--- task014.py
+++ task014.py
@@ -1,7 +1,7 @@
-# Номер автомобиля - r"[A-Z]\d{3}[A-Z]{2}\d{2,3}RUS"
+# Номер автомобиля - r"[A-Z]\d{3}[A-Z]{2}\d{2,3}"
 import re
-text = input("Введите номер автомобиля: ")
-number = re.compile(r'[А-Я]\d{3}[А-Я]{2}\d{2,3}RUS')
+text = input("Введите номер автомобиля (А123АА123): ")
+number = re.compile(r'[А-Я]\d{3}[А-Я]{2}\d{2,3}')
 if re.match(number,text):
     print("Номер указан верно")
 else:
